cdc58996c324665b7964a770da2df6d89e91c538
Anna-JuMacedo <annajuliamacedo16@gmail.com>
2025-06-25 11:43:27 -0300

    cartas dos pokemon

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-function pikachu1() {
-    document.getElementById("pokemon-name").innerHTML = "Pikachu " = pikachu;
+
+const pokemonImg = document.getElementById('pokemon-img');
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonType = document.getElementById('pokemon-type');
+const attacksList = document.getElementById('attacks-list');
+const card = document.getElementById('pokemon-card');
+
+function atualizarCard(pokemon) {
+    pokemonImg.src = pokemon.imagem;
+    pokemonImg.alt = pokemon.nome;
+    pokemonName.innerText = pokemon.nome;
+    pokemonType.innerText = `Tipo: ${pokemon.tipo}`;
+
+    attacksList.innerHTML = pokemon.ataques.map(ataque => `<li>${ataque}</li>`).join('');
+
+
+    card.className = 'pokemon-card ' + pokemon.tipo.toLowerCase();
 }
 
-function charmander1() {
-    document.getElementById("pokemon-name").innerHTML = "Charmander " = charmander;
+
+const pikachu = {
+    nome: 'Pikachu',
+    tipo: 'Elétrico',
+    imagem: 'images/pikachu.jpg',
+    ataques: ['Choque do Trovão', 'Ataque Rápido', 'Investida Elétrica']
+};
+
+const charmander = {
+    nome: 'Charmander',
+    tipo: 'Fogo',
+    imagem: 'images/charmander.jpg',
+    ataques: ['Lança-chamas', 'Garra de Fogo', 'Investida']
+};
+
+const squirtle = {
+    nome: 'Squirtle',
+    tipo: 'Água',
+    imagem: 'images/squirtle.jpg',
+    ataques: ["Jato d'Água", 'Bolhas', 'Cauda de Água']
+};
+
+
+function mostrarPikachu() {
+    atualizarCard(pikachu);
+}
+
+function mostrarCharmander() {
+    atualizarCard(charmander);
+}
+
+function mostrarSquirtle() {
+    atualizarCard(squirtle);
 }
